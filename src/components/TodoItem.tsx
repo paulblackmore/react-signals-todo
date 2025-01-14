c637e4c0f043f8bb4381fa44b24f7b944edeeb08
paul <paulmichaelblackmore@gmail.com>
2025-01-14 12:31:53 +0000

    feat: add styling and made TodoItem responsive

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -21,10 +21,23 @@ const TodoItem: React.FC<TodoItemProps> = ({
   };
 
   return (
-    <li>
-      <input type="checkbox" checked={todo.completed} onChange={handleToggle} />
-      <span className={todo.completed ? 'line-through' : ''}>{todo.text}</span>
-      <button onClick={handleDelete}>Delete</button>
+    <li className="flex justify-between items-center w-full my-4">
+      <div className="flex gap-2 item-center">
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={handleToggle}
+        />
+        <span className={todo.completed ? 'line-through' : ''}>
+          {todo.text}
+        </span>
+      </div>
+      <button
+        className="bg-transparent text-red-700 font-semibold hover:text-red-500 py-1 px-2 border border-red-500 hover:border-red-500 rounded"
+        onClick={handleDelete}
+      >
+        Delete
+      </button>
     </li>
   );
 };
